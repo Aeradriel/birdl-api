bcb580d69c4873530c05a76c5982060d2203d918
Thibaut Roche <thibaut.roche.perso@gmail.com>
2015-07-10 15:42:09 +0200

    [C]Messages

--- app/api/message_api.rb
+++ app/api/message_api.rb
@@ -2,7 +2,10 @@
 class MessageAPI < Grape::API
   desc 'Get all messages of user'
   get '/messages', each_serializer: MessageSerializer do
-    messages = @current_user.received_messages + @current_user.sent_messages
+    error!('Missing param "relation"', 400) if params[:relation].nil?
+    messages = @current_user.received_messages.where(
+        receiver_id: params[:relation].to_i) +
+        @current_user.sent_messages.where(receiver_id: params[:relation].to_i)
     messages.uniq
   end
 
