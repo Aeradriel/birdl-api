cb9098f3ed8cb549181dd6b4af883439f07a132b
Thibaut Roche <thibaut.roche.perso@gmail.com>
2015-07-12 12:31:13 +0200

    [C]Message serializer now display sender/receiver name

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,4 +1,12 @@
 class MessageSerializer < ActiveModel::Serializer
   root :message
-  attributes :id, :sender_id, :receiver_id, :content
+  attributes :id, :sender_id, :sender_name, :receiver_id, :receiver_name, :content
+
+  def sender_name
+    User.find(object.sender_id).name
+  end
+
+  def receiver_name
+    User.find(object.receiver_id).name
+  end
 end
