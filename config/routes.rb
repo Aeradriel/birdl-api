76f59ddb59d94ef1471270942481ef80f453412a
Thibaut Roche <thibaut.roche.perso@gmail.com>
2015-07-13 11:51:47 +0200

    [F]

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   mount API => '/'
   mount GrapeSwaggerRails::Engine => '/doc'
 
-  devise_for :user, skip: [:sessions]
+  devise_for :users, skip: [:sessions],
+             controllers: {
+                 registrations: 'users/registrations'
+             }
 end
