a48745d759d0dae1cd8bf33b09b3e1d7f52957cc
Thibaut Roche <thibaut.roche.perso@gmail.com>
2015-10-09 09:57:24 +0200

    [A]Event serializer now display free slots

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -2,5 +2,10 @@ class EventSerializer < ActiveModel::Serializer
   root :event
   attributes :id, :name, :type, :min_slots,
              :max_slots, :date, :end, :desc,
-             :owner_id, :address_id, :language, :location
+             :owner_id, :address_id, :language, :location,
+             :free_slots
+
+  def free_slots
+    object.max_slots - object.users.count - 1
+  end
 end
