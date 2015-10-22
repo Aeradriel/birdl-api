73ba4dcb7fe776ef870197c6f6482841f4ed2872
Thibaut Roche <thibaut.roche.perso@gmail.com>
2015-10-22 12:20:11 +0200

    [C]Event creation +ending_date +language

--- app/api/event_api.rb
+++ app/api/event_api.rb
@@ -40,14 +40,17 @@ class EventAPI < Grape::API
     error!('Missing param "min_slots"', 400) unless params[:min_slots]
     error!('Missing param "max_slots"', 400) unless params[:max_slots]
     error!('Missing param "date"', 400) unless params[:date]
+    error!('Missing param "ending_date"', 400) unless params[:ending_date]
     error!('Invalid event type', 400) unless Event.type.include?(params[:type])
 
     e = Event.new(name: params[:name], desc: params[:desc], type: params[:type],
-                  min_slots: params[:min_slots].to_i, max_slots: params[:max_slots].to_i)
+                  min_slots: params[:min_slots].to_i, max_slots: params[:max_slots].to_i,
+                  language: param[:language])
     begin
       e.date = Date.strptime(params['date'], '%Y/%m/%d')
-    rescue ArgumentError => e
-      error!(e.to_s, 400)
+      e.ending_date = Date.strptime(params['ending_date'], '%Y/%m/%d')
+    rescue ArgumentError => err
+      error!(err.to_s, 400)
     end
     if e.save
       e
--- app/models/face_to_face.rb
+++ app/models/face_to_face.rb
@@ -1,5 +1,4 @@
 # Class model for faces to faces
 class FaceToFace < Event
-  validates_presence_of :location
   validates_presence_of :language
 end
