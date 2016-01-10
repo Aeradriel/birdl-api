46cb6ba5224fede89dcea1ddf3a33be37855c085
Thibaut Roche <thibaut.roche.perso@gmail.com>
2016-01-10 13:04:47 +0100

    [A]PUT /notifications

--- app/api/notification_api.rb
+++ app/api/notification_api.rb
@@ -4,4 +4,13 @@ class NotificationAPI < Grape::API
   get '/notifications' do
     @current_user.notifications
   end
+
+  put '/notifications' do
+    @current_user.notifications.each do |n|
+      if n.update(seen: true) == false
+        error!('Could not update notification status', 500)
+      end
+    end
+    'OK'
+  end
 end
